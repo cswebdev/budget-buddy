1e9af11a31739454cbba60b96d89e85ebb13874f
cswebdev <csnider91@live.com>
2025-06-12 13:05:24 -0400

    fixed margin issue between signup button on form

--- web/src/app/singup/page.tsx
+++ web/src/app/singup/page.tsx
@@ -13,7 +13,7 @@ import { Separator } from "@/components/ui/separator"
 export default function SignupPage(){
     return (
         <div className="flex items-center justify-center min-h-screen">
-        <Card className="w-full max-w-md mx-auto mt-10">
+        <Card className="w-full max-w-md gap-4 mt-10">
             <CardHeader>
                 <CardTitle className="text-center my-3">Sign Up</CardTitle>
                 <CardDescription className="text-center text-sm text-gray-600">
@@ -23,13 +23,12 @@ export default function SignupPage(){
             <CardContent>
                 <RegistrationForm />
             </CardContent>
-            <CardFooter className="flex flex-col gap-2 justify-center">
-            <Separator className="my-1" />
+            <CardFooter className="flex flex-col justify-center gap-0">
                 {/* google one click  */}
-                <button className="w-full bg-gray-200 text-gray-800 py-2 rounded hover:bg-gray-300">
+                <button className="w-full bg-gray-200 text-gray-800 py-2 rounded hover:bg-gray-300 mb-0">
                     Sign up with Google
                 </button>
-                <a href="#" className="w-full text-center text-sm text-blue-500 hover:underline">
+                <a href="#" className="w-full text-center text-sm mt-3 text-blue-500 hover:underline">
                     Already have an account?
                 </a>
             </CardFooter>
--- web/src/components/registration/registration-form.tsx
+++ web/src/components/registration/registration-form.tsx
@@ -21,83 +21,85 @@ function handleShowPassword(event: React.MouseEvent<HTMLButtonElement>): void {
 
 export default function RegistrationForm() {
     return (
-        <form onSubmit={handleSubmit} className="registration-form">
-            <div className="relative gap-2 mb-6">
-                <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    required
-                    className="peer w-full px-3 py-2 border rounded focus:outline-none focus:ring-1 focus:ring-sky-500 placeholder-transparent"
-                    placeholder="Username"
-                />
-                <label
-                    htmlFor="username"
-                    className="
-                    absolute left-3 top-2 text-gray-500 bg-white px-1 transition-all duration-200
-                    pointer-events-none
-                    peer-placeholder-shown:top-2
-                    peer-placeholder-shown:text-base
-                    peer-placeholder-shown:text-gray-500
-                    peer-focus:-top-3
-                    peer-focus:text-md
-                    peer-focus:text-sky-500
-                    peer-focus:bg-white
-                    peer-not-placeholder-shown:-top-3
-                    peer-not-placeholder-shown:text-md
-                    peer-not-placeholder-shown:text-blue-500
-                    peer-not-placeholder-shown:bg-white
-                     "
-                >
-            Username
-                </label>
-            </div>
-            <div className="relative mb-6">
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    required
-                    className="peer w-full px-3 py-2 border rounded focus:outline-none focus:ring-1 focus:ring-sky-500 placeholder-transparent"
-                    placeholder="Password"
-                />
-                <label
-                    htmlFor="password"
-                    className="absolute left-3 top-2 text-gray-500 bg-white px-1 transition-all duration-200 
-                    pointer-events-none
-                     peer-placeholder-shown:top-2
-                    peer-placeholder-shown:text-base
-                    peer-placeholder-shown:text-gray-500
-                    peer-focus:-top-3
-                    peer-focus:text-md
-                    peer-focus:text-sky-500
-                    peer-focus:bg-white
-                    peer-not-placeholder-shown:-top-3
-                    peer-not-placeholder-shown:text-md
-                    peer-not-placeholder-shown:text-blue-500
-                    peer-not-placeholder-shown:bg-white
-                    "
-                >   
-                    Password
-                </label>
-                <button
-                    type="button"
-                    onClick={handleShowPassword}
-                    className="absolute right-3 top-2 text-gray-500 hover:text-gray-700 focus:outline-none hidden peer-focus:block peer-not-placeholder-shown:block"
-                >
-                      <Eye
-                    className="absolute right-3 text-gray-500 cursor-pointer hover:text-gray-700"
-                    
-                />
+        <div className="gap-4">
+            <form onSubmit={handleSubmit} className="registration-form">
+                <div className="relative mt-4 mb-8">
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        required
+                        className="peer w-full px-3 py-2 border rounded focus:outline-none focus:ring-1 focus:ring-sky-500 placeholder-transparent"
+                        placeholder="Username"
+                    />
+                    <label
+                        htmlFor="username"
+                        className="
+                        absolute left-3 top-2 text-gray-500 bg-white px-1 transition-all duration-200
+                        pointer-events-none
+                        peer-placeholder-shown:top-2
+                        peer-placeholder-shown:text-base
+                        peer-placeholder-shown:text-gray-500
+                        peer-focus:-top-3
+                        peer-focus:text-md
+                        peer-focus:text-sky-500
+                        peer-focus:bg-white
+                        peer-not-placeholder-shown:-top-3
+                        peer-not-placeholder-shown:text-md
+                        peer-not-placeholder-shown:text-blue-500
+                        peer-not-placeholder-shown:bg-white
+                        "
+                    >
+                Username
+                    </label>
+                </div>
+                <div className="relative mt-4 mb-8">
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                        className="peer w-full px-3 py-2 border rounded focus:outline-none focus:ring-1 focus:ring-sky-500 placeholder-transparent"
+                        placeholder="Password"
+                    />
+                    <label
+                        htmlFor="password"
+                        className="absolute left-3 top-2 text-gray-500 bg-white px-1 transition-all duration-200 
+                        pointer-events-none
+                        peer-placeholder-shown:top-2
+                        peer-placeholder-shown:text-base
+                        peer-placeholder-shown:text-gray-500
+                        peer-focus:-top-3
+                        peer-focus:text-md
+                        peer-focus:text-sky-500
+                        peer-focus:bg-white
+                        peer-not-placeholder-shown:-top-3
+                        peer-not-placeholder-shown:text-md
+                        peer-not-placeholder-shown:text-blue-500
+                        peer-not-placeholder-shown:bg-white
+                        "
+                    >   
+                        Password
+                    </label>
+                    <button
+                        type="button"
+                        onClick={handleShowPassword}
+                        className="absolute right-3 top-2 text-gray-500 hover:text-gray-700 focus:outline-none hidden peer-focus:block peer-not-placeholder-shown:block"
+                    >
+                        <Eye
+                        className="absolute right-3 text-gray-500 cursor-pointer hover:text-gray-700"
+                        
+                    />
+                    </button>
+                
+                </div>
+                <button className="w-full bg-green-600 text-white py-2 rounded hover:bg-blue-600 mt-2 mb-0
+                focus-bg-green-600
+                active" 
+                type="submit">
+                        Sign Up
                 </button>
-              
-            </div>
-            <button className="w-full bg-green-600 text-white py-2 rounded hover:bg-blue-600 mt-5
-            focus-bg-green-600
-            active" 
-            type="submit">
-                    Sign Up
-            </button>
-        </form>
+            </form>
+        </div>
     );
 }
